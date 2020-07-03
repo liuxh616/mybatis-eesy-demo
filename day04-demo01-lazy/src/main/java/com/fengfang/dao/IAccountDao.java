2b1e17233f3b58841ae44f88716b25d1a935ab8e
liuxh616 <liuxh616@126.com>
2020-07-03 17:31:44 +0800

    增加关于一对多使用延迟加载的demo测试

---
+++ day04-demo01-lazy/src/main/java/com/fengfang/dao/IAccountDao.java
@@ -0,0 +1,18 @@
+﻿package com.fengfang.dao;
+
+import com.fengfang.domain.Account;
+
+import java.util.List;
+
+/**
+ * @Description TODO
+ * @Author liuxh
+ * @Date 2020/7/3 17:16
+ **/
+public interface IAccountDao {
+    /**
+     * 查询所有用户数据
+     * @return
+     */
+    public List<Account> findAll();
+}
---
+++ day04-demo01-lazy/src/test/java/com/fengfang/test/AccountTest.java
@@ -0,0 +1,50 @@
+﻿package com.fengfang.test;
+
+import com.fengfang.dao.IAccountDao;
+import com.fengfang.domain.Account;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @Description TODO
+ * @Author liuxh
+ * @Date 2020/7/3 17:20
+ **/
+public class AccountTest {
+    private InputStream inputStream;
+    private SqlSession session;
+    private IAccountDao accountDao;
+
+    @Before
+    public void  init() throws IOException {
+        inputStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+        session = factory.openSession();
+        accountDao = session.getMapper(IAccountDao.class);
+    }
+
+    @After
+    public void destroyProperties() throws IOException {
+        session.commit();
+        session.close();
+        inputStream.close();
+    }
+
+    @Test
+    public void testFindAll(){
+        List<Account> accounts = accountDao.findAll();
+        for(Account account :accounts){
+            System.out.println(account);
+        }
+    }
+
+}
